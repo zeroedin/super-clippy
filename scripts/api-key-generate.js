678c01cf9d361b448a2eb4fee3603834255342b9
Steven Spriggs <steven.spriggs@gmail.com>
2023-11-09 16:12:36 -0500

    fix typo in api script

--- scripts/api-key-generate.js
+++ scripts/api-key-generate.js
@@ -5,7 +5,7 @@ import 'dotenv/config';
 
 const { APIGATEWAY_APIKEY, APIGATEWAY_ENDPOINT } = process.env;
 
-const content = `const clippy = document.querySelector('super-clippy);\n\nclippy.apiKey='${APIGATEWAY_APIKEY}';\nclippy.endpoint='${APIGATEWAY_ENDPOINT}';`;
+const content = `const clippy = document.querySelector('super-clippy');\n\nclippy.apiKey='${APIGATEWAY_APIKEY}';\nclippy.endpoint='${APIGATEWAY_ENDPOINT}';`;
 
 fs.writeFile('./demo/addApi.js', content, (err) => {
   if (err) throw err;
